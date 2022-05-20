febb8c74a5a4e43e8420a47bf4fd9aee2e657ab0
matrinkoszak <mateusz.ryfa97@protonmail.com>
2022-05-20 20:48:56 +0200

    ProperAPIProjectSetup

---
+++ BackEnd/WeatherAppAPI/WeatherAppAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WeatherAppAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BackEnd/WeatherAppAPI/WeatherAppAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WeatherAppAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
