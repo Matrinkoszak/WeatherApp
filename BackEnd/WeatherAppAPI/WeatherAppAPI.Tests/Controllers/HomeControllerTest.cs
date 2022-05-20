400cc6e07610adaf450f22702387de2f6169018c
matrinkoszak <mateusz.ryfa97@protonmail.com>
2022-05-20 20:01:12 +0200

    API_Project_Creation

---
+++ BackEnd/WeatherAppAPI/WeatherAppAPI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using WeatherAppAPI;
+using WeatherAppAPI.Controllers;
+
+namespace WeatherAppAPI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Przygotowanie
+            HomeController controller = new HomeController();
+
+            // Wykonanie
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Sprawdzenie
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ BackEnd/WeatherAppAPI/WeatherAppAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WeatherAppAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
