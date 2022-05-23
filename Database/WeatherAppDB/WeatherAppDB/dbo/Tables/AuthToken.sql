c2d5d1cb132d67f9baa42f4e82c949a65faac640
matrinkoszak <mateusz.ryfa97@protonmail.com>
2022-05-23 17:21:04 +0200

    TokenTable

---
+++ Database/WeatherAppDB/WeatherAppDB/dbo/Tables/AuthToken.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[authToken]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [creationDate] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    [terminationDate] DATETIME NOT NULL, 
+    [user_id] BIGINT NOT NULL, 
+    [code] NVARCHAR(155) NOT NULL, 
+    CONSTRAINT [FK_authToken_user] FOREIGN KEY ([user_id]) REFERENCES [User]([Id])  
+)
