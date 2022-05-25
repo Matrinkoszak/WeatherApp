ccbd3c65e6fea07da9d93bcbe4403ff2bf8e8dcf
matrinkoszak <mateusz.ryfa97@protonmail.com>
2022-05-25 19:06:04 +0200

    db_required_changes

--- Database/WeatherAppDB/WeatherAppDB/dbo/Tables/User.sql
+++ Database/WeatherAppDB/WeatherAppDB/dbo/Tables/User.sql
@@ -2,7 +2,7 @@
     [Id]           BIGINT        IDENTITY (1, 1) NOT NULL,
     [login]        NVARCHAR (50) NOT NULL,
     [display_Name] NVARCHAR (50) NULL,
-    [password]     NVARCHAR (50) NOT NULL,
+    [password]     NVARCHAR (254) NOT NULL,
     [isAdmin]      BIT           DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
